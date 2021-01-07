eb351d78d71d3448b69ab6fae1fba0a0e02c29e4
Jeremy McClintock <jeremymcclintock@hotmail.com>
2021-01-07 08:13:07 -0800

    MQTT c/c++ libs build properly but c++ lib does not link correctly, resulting in an undefined reference to MQTTReasonCode_toString.  main.cpp can build either c or c++ example depending on initial define in file.  C application runs correctly on Raspberry PI 4.  project_config.hpp not currently used.

---
+++ src/main.cpp
@@ -0,0 +1,415 @@
+#define BUILD_C_SAMPLE
+
+#ifdef BUILD_C_SAMPLE
+/*******************************************************************************
+ * Copyright (c) 2012, 2020 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ *
+ * The Eclipse Public License is available at 
+ *   https://www.eclipse.org/legal/epl-2.0/
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial contribution
+ *    Frank Pagliughi - loop to repeatedly read and sent time values.
+ *******************************************************************************/
+
+// This is a somewhat contrived example to show an application that publishes
+// continuously, like a data acquisition app might do. In this case, though,
+// we don't have a sensor to read, so we use the system time as the number
+// of milliseconds since the epoch to simulate a data input.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include "MQTTAsync.h"
+
+#if !defined(_WIN32)
+#include <unistd.h>
+#else
+#include <windows.h>
+#include <Minwinbase.h>
+#endif
+
+#if defined(_WRS_KERNEL)
+#include <OsWrapper.h>
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+#define snprintf _snprintf
+#endif
+
+
+// Better not to flood a public broker. Test against localhost.
+#define ADDRESS         "tcp://broker.hivemq.com:1883"
+
+#define CLIENTID        "UniqueGoblyGookID"
+#define TOPIC           "goofygumdrop"
+#define QOS             1
+#define TIMEOUT         10000L
+#define SAMPLE_PERIOD   1000L    // in ms
+
+volatile int finished = 0;
+volatile int connected = 0;
+
+void connlost(void *context, char *cause)
+{
+	MQTTAsync client = (MQTTAsync)context;
+	MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+	int rc;
+
+	printf("\nConnection lost\n");
+	printf("     cause: %s\n", cause);
+
+	printf("Reconnecting\n");
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
+	if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start connect, return code %d\n", rc);
+ 		finished = 1;
+	}
+}
+
+void onDisconnectFailure(void* context, MQTTAsync_failureData* response)
+{
+	printf("Disconnect failed\n");
+	finished = 1;
+}
+
+void onDisconnect(void* context, MQTTAsync_successData* response)
+{
+	printf("Successful disconnection\n");
+	finished = 1;
+}
+
+void onSendFailure(void* context, MQTTAsync_failureData* response)
+{
+	MQTTAsync client = (MQTTAsync)context;
+	MQTTAsync_disconnectOptions opts = MQTTAsync_disconnectOptions_initializer;
+	int rc;
+
+	printf("Message send failed token %d error code %d\n", response->token, response->code);
+	opts.onSuccess = onDisconnect;
+	opts.onFailure = onDisconnectFailure;
+	opts.context = client;
+	if ((rc = MQTTAsync_disconnect(client, &opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start disconnect, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void onSend(void* context, MQTTAsync_successData* response)
+{
+	// This gets called when a message is acknowledged successfully.
+}
+
+
+void onConnectFailure(void* context, MQTTAsync_failureData* response)
+{
+	printf("Connect failed, rc %d\n", response ? response->code : 0);
+	finished = 1;
+}
+
+
+void onConnect(void* context, MQTTAsync_successData* response)
+{
+	printf("Successful connection\n");
+	connected = 1;
+}
+
+int messageArrived(void* context, char* topicName, int topicLen, MQTTAsync_message* m)
+{
+	/* not expecting any messages */
+	return 1;
+}
+
+int64_t getTime(void)
+{
+	#if defined(_WIN32)
+		FILETIME ft;
+		GetSystemTimeAsFileTime(&ft);
+		return ((((int64_t) ft.dwHighDateTime) << 8) + ft.dwLowDateTime) / 10000;
+	#else
+		struct timespec ts;
+		clock_gettime(CLOCK_REALTIME, &ts);
+		return ((int64_t) ts.tv_sec * 1000) + ((int64_t) ts.tv_nsec / 1000000);
+	#endif
+}
+
+int main(int argc, char* argv[])
+{
+	MQTTAsync client;
+	MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+
+	MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
+	MQTTAsync_responseOptions pub_opts = MQTTAsync_responseOptions_initializer;
+
+	printf("Periodic time publishing MQTT client running...\n");
+	fflush(stdout);
+
+	int rc;
+
+	const char * pResponse = MQTTReasonCode_toString(MQTTREASONCODE_SUCCESS);
+
+	if ((rc = MQTTAsync_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to create client object, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((rc = MQTTAsync_setCallbacks(client, NULL, connlost, messageArrived, NULL)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to set callback, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
+	conn_opts.onSuccess = onConnect;
+	conn_opts.onFailure = onConnectFailure;
+	conn_opts.context = client;
+	if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start connect, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+
+	while (!connected) {
+		#if defined(_WIN32)
+			Sleep(100);
+		#else
+			usleep(100000L);
+		#endif
+		fflush(stdout);
+	}
+
+	while (!finished) {
+		int64_t t = getTime();
+
+		char buf[256];
+		int n = snprintf(buf, sizeof(buf), "Current time:%lld", (long long) t);
+		printf("%s\n", buf);
+
+		pub_opts.onSuccess = onSend;
+		pub_opts.onFailure = onSendFailure;
+		pub_opts.context = client;
+
+		pubmsg.payload = buf;
+		pubmsg.payloadlen = n;
+		pubmsg.qos = QOS;
+		pubmsg.retained = 0;
+
+		if ((rc = MQTTAsync_sendMessage(client, TOPIC, &pubmsg, &pub_opts)) != MQTTASYNC_SUCCESS)
+		{
+			printf("Failed to start sendMessage, return code %d\n", rc);
+			exit(EXIT_FAILURE);
+		}
+
+		fflush(stdout);
+
+		#if defined(_WIN32)
+			Sleep(SAMPLE_PERIOD);
+		#else
+			usleep(SAMPLE_PERIOD * 1000);
+		#endif
+	}
+
+	MQTTAsync_destroy(&client);
+ 	return rc;
+}
+#else
+
+#include <iostream>
+//#include "project_config.hpp"
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <thread>
+#include <chrono>
+#include "mqtt/async_client.h"
+
+using std::cout;
+using std::endl;
+
+const std::string SERVER_ADDRESS("tcp://broker.hivemq.com:1883");
+const std::string CLIENT_ID("totally_random_guy");
+const std::string TOPIC("goofygumdrop");
+
+const int	QOS = 1;
+const int	N_RETRY_ATTEMPTS = 5;
+
+// Callbacks for the success or failures of requested actions.
+// This could be used to initiate further action, but here we just log the
+// results to the console.
+
+class action_listener : public virtual mqtt::iaction_listener
+{
+	std::string name_;
+
+	void on_failure(const mqtt::token& tok) override {
+		std::cout << name_ << " failure";
+		if (tok.get_message_id() != 0)
+			std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
+		std::cout << std::endl;
+	}
+
+	void on_success(const mqtt::token& tok) override {
+		std::cout << name_ << " success";
+		if (tok.get_message_id() != 0)
+			std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
+		auto top = tok.get_topics();
+		if (top && !top->empty())
+			std::cout << "\ttoken topic: '" << (*top)[0] << "', ..." << std::endl;
+		std::cout << std::endl;
+	}
+
+public:
+	action_listener(const std::string& name) : name_(name) {}
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Local callback & listener class for use with the client connection.
+ * This is primarily intended to receive messages, but it will also monitor
+ * the connection to the broker. If the connection is lost, it will attempt
+ * to restore the connection and re-subscribe to the topic.
+ */
+class callback : public virtual mqtt::callback,
+					public virtual mqtt::iaction_listener
+
+{
+	// Counter for the number of connection retries
+	int nretry_;
+	// The MQTT client
+	mqtt::async_client& cli_;
+	// Options to use if we need to reconnect
+	mqtt::connect_options& connOpts_;
+	// An action listener to display the result of actions.
+	action_listener subListener_;
+
+	// This deomonstrates manually reconnecting to the broker by calling
+	// connect() again. This is a possibility for an application that keeps
+	// a copy of it's original connect_options, or if the app wants to
+	// reconnect with different options.
+	// Another way this can be done manually, if using the same options, is
+	// to just call the async_client::reconnect() method.
+	void reconnect() {
+		std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+		try {
+			cli_.connect(connOpts_, nullptr, *this);
+		}
+		catch (const mqtt::exception& exc) {
+			std::cerr << "Error: " << exc.what() << std::endl;
+			exit(1);
+		}
+	}
+
+	// Re-connection failure
+	void on_failure(const mqtt::token& tok) override {
+		std::cout << "Connection attempt failed" << std::endl;
+		if (++nretry_ > N_RETRY_ATTEMPTS)
+			exit(1);
+		reconnect();
+	}
+
+	// (Re)connection success
+	// Either this or connected() can be used for callbacks.
+	void on_success(const mqtt::token& tok) override {}
+
+	// (Re)connection success
+	void connected(const std::string& cause) override {
+		std::cout << "\nConnection success" << std::endl;
+		std::cout << "\nSubscribing to topic '" << TOPIC << "'\n"
+			<< "\tfor client " << CLIENT_ID
+			<< " using QoS" << QOS << "\n"
+			<< "\nPress Q<Enter> to quit\n" << std::endl;
+
+		cli_.subscribe(TOPIC, QOS, nullptr, subListener_);
+	}
+
+	// Callback for when the connection is lost.
+	// This will initiate the attempt to manually reconnect.
+	void connection_lost(const std::string& cause) override {
+		std::cout << "\nConnection lost" << std::endl;
+		if (!cause.empty())
+			std::cout << "\tcause: " << cause << std::endl;
+
+		std::cout << "Reconnecting..." << std::endl;
+		nretry_ = 0;
+		reconnect();
+	}
+
+	// Callback for when a message arrives.
+	void message_arrived(mqtt::const_message_ptr msg) override {
+		std::cout << "Message arrived" << std::endl;
+		std::cout << "\ttopic: '" << msg->get_topic() << "'" << std::endl;
+		std::cout << "\tpayload: '" << msg->to_string() << "'\n" << std::endl;
+	}
+
+	void delivery_complete(mqtt::delivery_token_ptr token) override {}
+
+public:
+	callback(mqtt::async_client& cli, mqtt::connect_options& connOpts)
+				: nretry_(0), cli_(cli), connOpts_(connOpts), subListener_("Subscription") {}
+};
+
+int main(int, char**) {
+    //cout << "Project name: " << PROJECT_NAME << " Version: " << PROJECT_VER << endl;
+
+    // A subscriber often wants the server to remember its messages when its
+	// disconnected. In that case, it needs a unique ClientID and a
+	// non-clean session.
+
+	mqtt::async_client cli(SERVER_ADDRESS, CLIENT_ID);
+
+	mqtt::connect_options connOpts;
+	connOpts.set_clean_session(false);
+
+	// Install the callback(s) before connecting.
+	callback cb(cli, connOpts);
+	cli.set_callback(cb);
+
+	// Start the connection.
+	// When completed, the callback will subscribe to topic.
+
+	try {
+		std::cout << "Connecting to the MQTT server..." << std::flush;
+		cli.connect(connOpts, nullptr, cb);
+	}
+	catch (const mqtt::exception& exc) {
+		std::cerr << "\nERROR: Unable to connect to MQTT server: '"
+			<< SERVER_ADDRESS << "'" << exc << std::endl;
+		return 1;
+	}
+
+	// Just block till user tells us to quit.
+
+	while (std::tolower(std::cin.get()) != 'q')
+		;
+
+	// Disconnect
+
+	try {
+		std::cout << "\nDisconnecting from the MQTT server..." << std::flush;
+		cli.disconnect()->wait();
+		std::cout << "OK" << std::endl;
+	}
+	catch (const mqtt::exception& exc) {
+		std::cerr << exc << std::endl;
+		return 1;
+	}
+
+ 	return 0;
+}
+
+#endif
